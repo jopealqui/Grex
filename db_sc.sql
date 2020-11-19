1892f5a8cf80574a9e6cc67ade73ede9c260c2bd
João Pedro A. Quinaglia <jopealqui@outlook.com>
2020-11-19 15:07:43 -0300

    Add files via upload

--- db_sc.sql
+++ db_sc.sql
@@ -12,7 +12,17 @@ CREATE TABLE Usuario (
     PRIMARY KEY (ID_User)
 ) CHARSET = utf8 ;
 
-CREATE TABLE Usuario (
+CREATE TABLE Carro (
+    ID_Carro INT NOT NULL AUTO_INCREMENT,
+    Modelo TEXT NOT NULL,
+    Ano TEXT NOT NULL,
+    Placa TEXT NOT NULL,
+    Lugares TEXT NOT NULL,
+    Cor TEXT NOT NULL,
+    PRIMARY KEY (ID_Carro)
+) CHARSET = utf8;
+
+CREATE TABLE Motorista (
     ID_Motorista INT NOT NULL AUTO_INCREMENT,
     Nome TEXT NOT NULL,
     Email VARCHAR (200) NOT NULL,
@@ -22,24 +32,15 @@ CREATE TABLE Usuario (
     CNH INT NOT NULL,
     RG  INT NOT NULL,
     CPF INT NOT NULL,
+    Avaliacao FLOAT,
     CONSTRAINT fk_ID_Carro FOREIGN KEY (ID_Carro) REFERENCES Carro (ID_Carro),
-    PRIMARY KEY (ID_User)
+    PRIMARY KEY (ID_Motorista)
 ) CHARSET = utf8 ;
-
-CREATE TABLE Carro (
-    ID_Carro INT NOT NULL AUTO_INCREMENT,
-    Modelo TEXT NOT NULL,
-    Ano TEXT NOT NULL,
-    Placa TEXT NOT NULL,
-    Lugares TEXT NOT NULL,
-    Cor TEXT NOT NULL,
-    PRIMARY KEY (ID_User)
-) CHARSET = utf8;
 
 CREATE TABLE Localização (
     ID_Localização INT NOT NULL AUTO_INCREMENT,
     Logradouro TEXT NOT NULL,
     Número INT NOT NULL,
     CEP INT NOT NULL,
-    PRIMARY KEY (ID_User)
+    PRIMARY KEY (ID_Localização)
 ) CHARSET = utf8;
